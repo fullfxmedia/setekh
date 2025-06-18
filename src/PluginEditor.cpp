8bd942a7038ece9e1eac1606abf795e1c94e9555
Sean Chambers <schambers80@gmail.com>
2025-06-17 20:55:19 -0500

    reposition knobs

--- src/PluginEditor.cpp
+++ src/PluginEditor.cpp
@@ -7,28 +7,39 @@ SetekhAudioProcessorEditor::SetekhAudioProcessorEditor(SetekhAudioProcessor& p)
     
   amountLabel.setText("Amount", juce::dontSendNotification);
   mixLabel.setText("Mix", juce::dontSendNotification);
+
+  amountSlider.setSliderStyle(juce::Slider::RotaryHorizontalVerticalDrag);
+  amountSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 60, 20);
+
+  mixSlider.setSliderStyle(juce::Slider::RotaryHorizontalVerticalDrag);
+  mixSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 50, 18);
   
   for (auto* comp : { &amountSlider, &mixSlider }) {
       addAndMakeVisible(*comp);
-      comp->setSliderStyle(juce::Slider::Rotary);
-      comp->setTextBoxStyle(juce::Slider::TextBoxBelow, false, 60, 20);
   }
 
   for (auto* label : { &amountLabel, &mixLabel }) {
       addAndMakeVisible(*label);
   }
 
-  setSize(300, 200);
+  setSize(250, 400);
 }
 
 void SetekhAudioProcessorEditor::paint(juce::Graphics& g) {
   g.fillAll(juce::Colours::black);
 }
 
-void SetekhAudioProcessorEditor::resized() {
-  amountSlider.setBounds(40, 40, 100, 100);
-  amountLabel.setBounds(40, 20, 100, 20);
+void SetekhAudioProcessorEditor::resized()
+{
+    // Large centered Amount knob
+    int centerX = getWidth() / 2;
+    int amountKnobSize = 150;
+    amountSlider.setBounds(centerX - amountKnobSize / 2, 60, amountKnobSize, amountKnobSize);
+    amountLabel.setBounds(centerX - 50, 40, 100, 20);
 
-  mixSlider.setBounds(160, 40, 100, 100);
-  mixLabel.setBounds(160, 20, 100, 20);
+    // Smaller Mix knob in bottom-right
+    int mixKnobSize = 80;
+    int padding = 20;
+    mixSlider.setBounds(getWidth() - mixKnobSize - padding, getHeight() - mixKnobSize - 2 * padding, mixKnobSize, mixKnobSize);
+    mixLabel.setBounds(mixSlider.getX(), mixSlider.getY() - 20, mixKnobSize, 20);
 }
